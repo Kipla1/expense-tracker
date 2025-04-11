a4eca0213620477cdd47c5f5dbb5c2d4983bfc09
Oscar <oscartrent6@gmail.com>
2025-04-11 15:21:27 +0300

    launch

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import './App.css'
+import Table from './Table'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <h1 className='head'>EXPENSE TRACKER</h1>
+        <p className='head-em'>*start taking control of your finances</p>
+        <Table></Table>
+         
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Table.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+const Table = () => {
+  return (
+    <div>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>  Expense    </th>
+            <th>    Description   </th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  )
+}
+
+export default Table
